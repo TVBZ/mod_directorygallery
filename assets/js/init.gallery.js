ea1027649ea9dd424cb705f153c9ddf4042a8916
TVBZ <43469713+TVBZ@users.noreply.github.com>
2020-08-06 01:26:01 +0200

    fix IE11 compatability issue

--- assets/js/init.gallery.js
+++ assets/js/init.gallery.js
@@ -5,51 +5,53 @@
  */
 
 
+"use strict";
+
 // Get module params
-const dgModuleSettings = Joomla.getOptions('mod_directorygallery');
+var dgModuleSettings = Joomla.getOptions('mod_directorygallery');
 
 jQuery(document).ready(function () {
-
+    
     // Loop over each module
-    const dgmodules = Object.keys(dgModuleSettings);
-    for (const id of dgmodules) {
-
-        const params = dgModuleSettings[id];
-        const target = "#light-gallery-" + id;
-
+    var dgmodules = Object.keys(dgModuleSettings);
+    for (var _i = 0, _dgmodules = dgmodules; _i < _dgmodules.length; _i++) {
+        var id = _dgmodules[_i];
+        var params = dgModuleSettings[id];
+        var target = "#light-gallery-" + id; 
+        
         // Initialize gallery
-        const output = {
+        var output = {
             selector: ".lg-img-container",
             mode: params.transition,
-            getCaptionFromTitleOrAlt: (params.caption == 1),
-            thumbnail: (params.thumbnails == 1),
+            getCaptionFromTitleOrAlt: params.caption == 1,
+            thumbnail: params.thumbnails == 1,
             animateThumb: true,
             thumbMargin: params.thumbnails_margin,
-            showThumbByDefault: (params.thumbnails_show == 1),
-            toogleThumb: (params.thumbnails_toggle == 1),
-            counter: (params.counter == 1),
-            download: (params.download == 1),
-            fullScreen: (params.fullscreen == 1),
-            zoom: (params.zoom == 1),
-            actualSize: (params.actualsize == 1),
+            showThumbByDefault: params.thumbnails_show == 1,
+            toogleThumb: params.thumbnails_toggle == 1,
+            counter: params.counter == 1,
+            download: params.download == 1,
+            fullScreen: params.fullscreen == 1,
+            zoom: params.zoom == 1,
+            actualSize: params.actualsize == 1,
             scale: params.scale,
-            share: (params.share == 1),
-            hash: (params.hash == 1),
-            autoplay: (params.autoplay == 1),
-            autoplayControls: (params.autoplaycontrols == 1),
+            share: params.share == 1,
+            hash: params.hash == 1,
+            autoplay: params.autoplay == 1,
+            autoplayControls: params.autoplaycontrols == 1,
             speed: params.speed,
             pause: params.pauze,
-            loop: (params.loop == 1),
+            loop: params.loop == 1,
             hideControlOnEnd: true,
-            progressBar: (params.progressbar == 1),
-            mouseWheel: (params.mousewheel == 1),
-            pager: (params.pager == 1)
-        };
-
+            progressBar: params.progressbar == 1,
+            mouseWheel: params.mousewheel == 1,
+            pager: params.pager == 1
+        }; 
+        
         //console.log(output);
         jQuery(target).lightGallery(output);
         console.log(target + " initialized.");
+    }
 
-    };
-
+    ;
 });
